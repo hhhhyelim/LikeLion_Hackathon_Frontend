ff6be8cd2712e216a1999bc6d61a9476a088f5a7
hhhhyelim <yanghl0527@naver.com>
2023-08-14 19:33:09 +0900

    Feat. 마이페이지 화면 생성

---
+++ src/screens/Mypage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import { Text, SafeAreaView } from 'react-native';
+
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.background};
+`;
+
+
+const TitleContainer = styled.View`
+  align-items: center;
+  margin-top: 23%;
+`;
+
+
+const Line = styled.View`
+  width: 93%;
+  height: 2px;
+  background-color: black;
+  margin-top: 10px;
+  border-radius: 10px;
+`;
+
+const Mypage = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Container>
+        <TitleContainer>
+          <Text style={{ fontSize: 20, fontWeight: '500' }}>마이페이지</Text>
+          <Line />
+        </TitleContainer>
+        
+        
+      </Container>
+      
+    </SafeAreaView>
+  );
+};
+
+export default Mypage;
